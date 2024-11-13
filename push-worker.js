8d2ad6c92eeff077acc4090da2033d15e3c72604
Mavox-ID <markd.voznyuk@gmail.com>
2024-11-13 17:46:35 +0200

    Updated website design

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `9f8a4712072d874ada30e5f40727c31f`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
